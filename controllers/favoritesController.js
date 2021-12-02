9f587aeb402be957a0ac1265ef7dfdcb64056e13
KevinTortoledoRodriguez <susetsoto05@gmail.com>
2021-12-01 23:43:06 -0700

    Update favoritesController.js

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -4,27 +4,28 @@ export const getFavorites = async (req, res) => {
   await pool.query('SELECT * FROM FAVORITES', function(error, results){
     if(error){
       throw error;
-      res.json(results)
     }
+    res.json(results)
   })
 }
 
 export const postFavorites = async (req, res) => {
+  console.log(req.body)
   const { IdFild } = req.body
   await pool.query(`INSERT INTO FAVORITES (IdFild) values (${IdFild})`, function(error, results){
     if(error){
       throw error;
-      res.json(results)
     }
+    res.json(results)
   })
 }
 
 export const deleteFavorites = async (req, res) => {
-  const { ID } = req.body
-  await pool.query(`DELETE FROM FAVORITES WHERE =${ID}`, function(error, results){
+  const ID = req.params.id
+  await pool.query(`DELETE FROM FAVORITES WHERE ID=${ID}`, function(error, results){
     if(error){
       throw error;
-      res.json(results)
     }
+    res.json(results)
   })
 }
