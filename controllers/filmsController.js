53e4f67553bee537e502d04bb094bf5cb643ed3d
KevinTortoledoRodriguez <ev.melissa147@gmail.com>
2021-12-01 23:55:53 -0700

    Update filmsController.js

--- controllers/filmsController.js
+++ controllers/filmsController.js
@@ -1,51 +1,52 @@
 import pool from '../db/mysql'
 
 export const getFilms = async (req, res) => {
-  await pool.query('SELECT * FROM FILMS', function(error, results){
+  await pool.query('SELECT * FROM FILM', function(error, results){
     if(error){
       throw error;
-      res.json(results)
     }
+    res.json(results)
   })
 }
 
 export const getFilm = async (req, res) => {
-  const { ID } = req.body
-  await pool.query(`SELECT * FROM FILMS WHERE ID = ${ID}`, function(error, results){
+  const ID = req.params.id
+  await pool.query(`SELECT * FROM FILM WHERE ID = ${ID}`, function(error, results){
     if(error){
       throw error;
-      res.json(results)
     }
+    res.json(results)
   })
 }
 
 export const postFilms = async (req, res) => {
-  const { Name, Director, IdGenre, Descripcion, Type, Year, Review, Score } = req.body
-  await pool.query(`INSERT INTO FILMS (Name, Director, IdGenre, Descripcion, Type, Year, Review, Score, Director) 
-  values ('${Name}','${Director}','${IdGenre}','${Descripcion}','${Type}','${Year}','${Review}','${Score}')`, function(error, results){
+  const { Name, Director, IdGenre, Description, Type, Year, Review, Score } = req.body
+  await pool.query(`INSERT INTO FILM (Name, Director, IdGenre, Description, Type, Year, Review, Score) 
+  values ('${Name}','${Director}','${IdGenre}','${Description}','${Type}','${Year}','${Review}','${Score}')`, function(error, results){
     if(error){
       throw error;
-      res.json(results)
     }
+    res.json(results)
   })
 }
 
 export const putFilms = async (req, res) => {
-  const { ID, Descripcion, Type, Year, Review, Score } = req.body
-  await pool.query(`UPDATE FILMS SET  WHERE =${ID} SET Descripcion='${Descripcion}', Type='${Type}', Year='${Year}', Review='${Review}', Score=${Score} WHERE ID=${ID}`, function(error, results){
+  const ID = req.params.id
+  const { Description, Type, Year, Review, Score } = req.body
+  await pool.query(`UPDATE FILM SET Description='${Description}', Type='${Type}', Year='${Year}', Review='${Review}', Score=${Score} WHERE ID=${ID}`, function(error, results){
     if(error){
       throw error;
-      res.json(results)
     }
+    res.json(results)
   })
 }
 
 export const deleteFilms = async (req, res) => {
-  const { ID } = req.body
-  await pool.query(`DELETE FROM FILMS WHERE =${ID}`, function(error, results){
+  const ID = req.params.id
+  await pool.query(`DELETE FROM FILM WHERE ID=${ID}`, function(error, results){
     if(error){
       throw error;
-      res.json(results)
     }
+    res.json(results)
   })
 }
